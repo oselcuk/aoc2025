b722d4a55b84df2be8db4523d62c78d039de77b5
Ozy <github@oselcuk.com>
2025-12-05 11:03:19 +0100

    yum

---
+++ day05/main.rb
@@ -0,0 +1,23 @@
+
+
+ranges, ids = File.read(ARGV[0]).split("\n\n").map{|text| text.split("\n")}
+ids = ids.map &:to_i
+
+def combine_range(a, b) [a.first, b.first].min .. [a.last, b.last].max end
+
+ranges = ranges.map{|range| range.split("-").map(&:to_i)}.sort.map{|i, j| i..j}
+  .reduce [] do |(*list, last), range|
+    if last == nil then
+      [range]
+    elsif last.overlap? range then
+      [*list, combine_range(last, range)]
+    else
+      [*list, last, range]
+    end
+  end
+
+part1 = ids.filter {|id| ranges.any? {|range| range.cover? id}}.length
+part2 = ranges.map(&:count).sum
+
+puts "Part 1: #{part1}"
+puts "Part 2: #{part2}"
